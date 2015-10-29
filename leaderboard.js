9be1d828ca27fa2b493f226372a3f56b890622e6
Kelsie Yamakawa <kayama@hawaii.edu>
2015-10-28 16:10:18 -1000

    Finished WOD

--- leaderboard.js
+++ leaderboard.js
@@ -1,16 +1,24 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient) {
+
+  Meteor.subscribe('thePlayers');
+
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find()
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({}, {sort: {score: -1, name: 1} });
     },
     'selectedClass': function(){
       var playerId = this._id;
       var selectedPlayer = Session.get('selectedPlayer');
       if(playerId == selectedPlayer){
-        return "selected"
+        return "selected";
       }
+    },
+    'showSelectedPlayer': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
 
@@ -21,15 +29,49 @@ if(Meteor.isClient) {
     },
     'click .increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
     },
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+    },
+    'click .remove': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('removePlayerData', selectedPlayer);
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function() {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      Meteor.call('insertPlayerData', playerNameVar);
     }
   });
 }
 
-if(Meteor.isServer){
+if(Meteor.isServer) {
+  Meteor.publish('thePlayers', function () {
+    var currentUserId = this.userId;
+    return PlayersList.find({createdBy: currentUserId});
+  });
 
+  Meteor.methods({
+    'insertPlayerData': function (playerNameVar) {
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer) {
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue) {
+      var currentUserId = Meteor.userId();
+      PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue}});
+    }
+  });
 }
